55d58b73cc1ac6161aae93bfe01dac69acfe4654
Naitik Shah <n@daaku.org>
2012-05-04 17:12:34 -0700

    initial commit

---
+++ httpstats.go
@@ -0,0 +1,27 @@
+// Package provides a HTTP handler that will keep track of some useful
+// request statistics and expose them via expvar.
+package httpstats
+
+import (
+	"expvar"
+	"net/http"
+)
+
+type Handler struct{
+	Handler http.Handler
+	Name string
+	totalRequests *expvar.Int
+}
+
+func NewHandler(name string, handler http.Handler) *Handler {
+	return &Handler{
+		Handler: handler,
+		Name: name,
+		totalRequests: expvar.NewInt(name + "_total_requests"),
+	}
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.totalRequests.Add(1)
+	h.Handler.ServeHTTP(w, r)
+}
